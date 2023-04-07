7ccb4909fd0c9aaf227d29f8f95a34215b66720f
Yury0310 <ostap4uk41@gmail.com>
2023-04-08 01:13:58 +0300

    done pop_up main

--- shelter/js/index.js
+++ shelter/js/index.js
@@ -8,11 +8,114 @@ console.log(`Страница Main (60):\n
 6. Вёрстка соответствует макету +15\n
 7. Требования к css +4\n
 8. Интерактивность элементов +14`);
-import { pets_info } from "./pets_info.js";
-const petCards = document.querySelectorAll(".pet_card_item");
-console.log(petCards);
-pets_info.forEach((pet_info) => {
-  petCards.forEach((card) => {
-    card.addEventListener("click", () => {});
+
+import { pets } from "./pets_info.js";
+
+const pet_cards = document.querySelector(".pet_cards");
+
+pets.forEach((pet_info) => {
+  const pet_card = document.createElement("div");
+  pet_card.className = "pet_card_item";
+  pet_cards.appendChild(pet_card);
+
+  const pet_img = document.createElement("img");
+  pet_img.className = "pet_img";
+  pet_img.src = pet_info.img;
+  pet_card.appendChild(pet_img);
+
+  const pet_name = document.createElement("h4");
+  pet_name.textContent = pet_info.name;
+  pet_card.appendChild(pet_name);
+
+  const button = document.createElement("button");
+  button.textContent = "Learn more";
+  pet_card.appendChild(button);
+
+  pet_card.addEventListener("click", () => {
+    document.body.classList.add("lock");
+
+    const pop_up = document.createElement("div");
+    pop_up.className = "pop-up";
+    document.body.appendChild(pop_up);
+
+    const close_div = document.createElement("div");
+    close_div.className = "close-btn";
+    const close_btn = document.createElement("button");
+    close_div.appendChild(close_btn);
+    pop_up.appendChild(close_div);
+
+    const popUpContent = document.createElement("div");
+    popUpContent.className = "pop-up_content";
+    pop_up.appendChild(popUpContent);
+
+    const popUpImg = document.createElement("div");
+    popUpImg.className = "pop-up_img";
+    const ImgPopUpImg = document.createElement("img");
+    ImgPopUpImg.src = pet_info.img;
+    popUpImg.appendChild(ImgPopUpImg);
+    popUpContent.appendChild(popUpImg);
+
+    const popUpPetInfo = document.createElement("div");
+    popUpPetInfo.className = "pop-up_pet_info";
+    popUpContent.appendChild(popUpPetInfo);
+
+    const pet_name = document.createElement("h3");
+    pet_name.textContent = pet_info.name;
+    popUpPetInfo.appendChild(pet_name);
+
+    const pet_type = document.createElement("h4");
+    pet_type.textContent = `${pet_info.type} - ${pet_info.breed}`;
+    popUpPetInfo.appendChild(pet_type);
+
+    const pet_descr = document.createElement("p");
+    pet_descr.textContent = pet_info.description;
+    popUpPetInfo.appendChild(pet_descr);
+
+    const ul = document.createElement("ul");
+    popUpPetInfo.appendChild(ul);
+
+    const li1 = document.createElement("li");
+    li1.className = "li_item";
+    li1.innerHTML = `<strong>Age:</strong>&nbsp ${pet_info.age}`;
+    ul.appendChild(li1);
+
+    const li2 = document.createElement("li");
+    li2.className = "li_item";
+    li2.innerHTML = `<strong>Inoculations:</strong>&nbsp ${pet_info.inoculations}`;
+    ul.appendChild(li2);
+
+    const li3 = document.createElement("li");
+    li3.className = "li_item";
+    li3.innerHTML = `<strong>Diseases:</strong>&nbsp ${pet_info.diseases}`;
+    ul.appendChild(li3);
+
+    const li4 = document.createElement("li");
+    li4.className = "li_item";
+    li4.innerHTML = `<strong>Parasites:</strong>&nbsp ${pet_info.parasites}`;
+    ul.appendChild(li4);
+
+    popUpContent.addEventListener("mouseleave", () => {
+      pop_up.style.cursor = "pointer";
+      close_btn.classList.add("hover");
+    });
+
+    popUpContent.addEventListener("mouseenter", () => {
+      pop_up.style.cursor = "default";
+      close_btn.classList.remove("hover");
+    });
+
+    pop_up.addEventListener("click", () => {
+      if (!popUpContent.contains(event.target)) {
+        document.body.classList.remove("lock");
+        pop_up.remove();
+      }
+    });
+    document.addEventListener("mouseleave", () => {
+      close_btn.classList.remove("hover");
+    });
+
+    document.addEventListener("mouseenter", () => {
+      close_btn.classList.add("hover");
+    });
   });
 });
